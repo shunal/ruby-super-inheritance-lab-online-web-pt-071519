260e0f21ecb23d07e117d97866a92cf50ff29b42
Steven Hunal <stevehunal@gmail.com>
2019-08-09 17:33:52 +0000

    Automatically backed up by Learn

---
+++ lib/chatty_student.rb
@@ -0,0 +1,14 @@
+require_relative "./student.rb"
+class ChattyStudent < Student 
+  
+  def hello
+    super
+    puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch the Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you dont want any spoilers? Okay well let me just tell you who died..."
+  end 
+  
+  def raise_hand
+    10.time.super
+    
+  end 
+  
+end 
